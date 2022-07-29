8d906392ad1195b950150c18fb5b11877c6a764c
ruslan.svr@mail.ru <ruslansvr@mail.ru>
2022-07-29 15:31:56 +0300

    8

---
+++ 8.cpp
@@ -0,0 +1,42 @@
+#include <string>
+#include <iostream>
+
+class Solution 
+{
+public:
+    Solution()
+    {
+        std::ios::sync_with_stdio(false);
+        std::cin.tie(nullptr);
+    }
+    int myAtoi(std::string s) 
+    {
+        int i;
+        long long value = 0;
+        int sign = 1;
+
+        while (isspace(s[i]))
+            i++;
+        if (s[i] == '+' || s[i] == '-') 
+        {
+            if( s[i] == '-' ) 
+                sign = -1;
+            i++;
+        }
+        while (isdigit(s[i]))
+        {
+            value *= 10;
+            value += (int) (s[i] - '0');
+            i++;
+            if (value > INT_MAX)
+                return (sign > 0) ? INT_MAX : INT_MIN;
+        }
+        return value * sign;
+    }
+};
+
+int main()
+{
+    Solution s;
+    std::cout << s.myAtoi("   +2147483647") << std::endl << s.myAtoi("   -2147483648");
+}
