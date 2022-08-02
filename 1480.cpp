0b0366df9efd4ba3226ee16df8339b5f06b15bd9
ruslan.svr@mail.ru <ruslansvr@mail.ru>
2022-08-02 18:58:29 +0300

    727 and 1480

---
+++ 1480.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> runningSum(vector<int>& nums) 
+    {
+        vector<int> res;
+
+        res.push_back(nums[0]);
+        for (int i = 1; i < nums.size(); i++)
+        {
+            res.push_back(nums[i] + res[i - 1]);
+        }
+        return res;
+    }
+};
